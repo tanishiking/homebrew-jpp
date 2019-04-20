34e3dc7004a7857d026e8a90cbeb4b8b20bc26f3
tanishiking24 <rikiriki1238@gmail.com>
2019-04-20 11:02:57 +0900

    v0.0.5

--- jpp.rb
+++ jpp.rb
@@ -3,14 +3,14 @@ require 'formula'
 class Jpp < Formula
   desc "JSON Prettier Printer that occupies a minimal number of lines while pretty-printing given JSON."
   homepage "https://godoc.org/github.com/tanishiking/jpp"
-  version '0.0.4'
+  version '0.0.5'
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/tanishiking/jpp/releases/download/v0.0.4/jpp_darwin_amd64.zip"
-    sha256 "d963af92412a5860139536df2c8b06aa71a7b1d36ad544e8451757abb2670cfa"
+    url "https://github.com/tanishiking/jpp/releases/download/v0.0.5/jpp_darwin_amd64.zip"
+    sha256 "49cd23f691438c21f6123bf076a6a9d74d31a8c3b6ab960e0fc7745498d55a0e"
   else
-    url "https://github.com/tanishiking/jpp/releases/download/v0.0.4/jpp_darwin_386.zip"
-    sha256 "1c4cb430414f07b5ccf33ca289952b308854a1afaf348c4bcbe2f48117a6e6cb"
+    url "https://github.com/tanishiking/jpp/releases/download/v0.0.5/jpp_darwin_386.zip"
+    sha256 "761f1796641dcf35ab4982a9937af06cdaca4ed86e54466f99350342ce806b3f"
   end
 
   def install
